12470dd809c43968e3150218763cb2d5fc83944d
Tommi Kurjensalo <tommi.kurjensalo@myy.haaga-helia.fi>
2017-04-01 19:02:33 +0300

    Update init.pp

--- hello/manifests/init.pp
+++ hello/manifests/init.pp
@@ -1,4 +1,8 @@
 class hello {
+
+	# Luodaan /tmp/helloModule tiedosto, jonka sisällä on teksti 'Hello World!'
+	# Varmistetaan, että checksum tyyppi on md5. 
+	# Määritetään myös tiedoston oikeudet 0744 = u+rwx g+rwx o+r
 	file { '/tmp/helloModule':
 		content => "Hello World!\n",
 		checksum => "md5",
