98ed79521ab362d33ac136139019ddb92493eefa
Tommi Kurjensalo <tommi.kurjensalo@myy.haaga-helia.fi>
2017-04-18 21:59:00 +0300

    Apache puppet module v2

--- doBase.sh
+++ doBase.sh
@@ -1,12 +1,20 @@
 # !/bin/bash
 # LiveUSB alkuasetuksien asennus scripti
-# Versio 12042017.2
+# Versio 18042017.2
 
 # Poistetaan nykyinen apt repository lista ja lisätään uudet lähteet
 echo ""
 echo "* Poistetaan nykyinen apt repository lista ja lisätään uudet lähteet *"
 echo ""
+
 sudo rm /etc/apt/sources.list && sudo touch /etc/apt/sources.list
+OUT=$?
+if [ $OUT = 0 ]; then
+  echo '*** [ Poistetttu sources.list && luotu uusi ]***'
+else
+  echo '*** [ sources.list poisto epäonnistui ]***'
+fi
+
 sudo apt-add-repository "deb http://se.archive.ubuntu.com/ubuntu/ yakkety main restricted universe multiverse"
 sudo apt-add-repository "deb http://se.archive.ubuntu.com/ubuntu/ yakkety-updates main restricted universe multiverse"
 sudo apt-add-repository "deb http://se.archive.ubuntu.com/ubuntu/ yakkety-security main restricted universe multiverse"
@@ -24,6 +32,13 @@ echo "* Konfiguroidaan käyttäjäkohtaiset git asetukset *"
 echo ""
 git config --global user.email "tommi.kurjensalo@myy.haaga-helia.fi"
 git config --global user.name "Tommi Kurjensalo"
+OUT=$?
+if [ $OUT = 0 ]; then
+  echo '***[ GIT asetukset konfiguroitu ]***'
+else
+  echo '***[ GIT asetuksien konfigurointi epäonnistui ]***'
+fi
+
 
 # Kloonataan keskitettyHallinta repository
 echo ""
@@ -33,18 +48,59 @@ cd ~
 git clone https://github.com/TommiKurjensalo/keskitettyHallinta.git
 cd keskitettyHallinta/
 
-# Kopioidaan puppet moduulit ja config tiedosto
+# Kopioidaan tarvittavat tiedostot & luodaan puppet/modules linkki
 echo ""
-echo "* Kopioidaan puppet moduulit ja config tiedostot *"
-echo "* Kopioidaan globaali gitconfig => /etc/gitconfig *"
+echo "* Kopioidaan:							*"
+echo "*  - globaali gitconfig => /etc/gitconfig 			*"
+echo "*  - bash_aliases 						*"
+echo "* Tehdään linkki ~/keskitettyHallinta => /etc/puppet/modules 	*"
 echo ""
-sudo cp -R ~/keskitettyHallinta/modules/ /etc/puppet/modules
+
+
 sudo cp ~/keskitettyHallinta/conf/puppet.conf /etc/puppet/puppet.conf
+OUT=$?
+if [ $OUT = 0 ]; then
+  echo '**[ Kopioitu puppet.conf onnistuneesti ]***'
+else
+  echo '**[ puppet.conf kopioiminen epäonnistui ]***'
+fi
+
+
 sudo cp ~/keskitettyHallinta/modules/git/templates/gitconfig.erb /etc/gitconfig
+OUT=$?
+if [ $OUT = 0 ]; then
+  echo '***[ Kopioitu /etc/gitconfig onnistuneesti ]***'
+else
+  echo '***[ /etc/gitconfig kopioiminen epäonnistui ]***'
+fi
+
 cp ~/keskitettyHallinta/conf/bash_aliases ~/.bash_aliases
+OUT=$?
+if [ $OUT = 0 ]; then
+  echo '***[ Kopioitu ~./bash_aliases onnistuneesti ]***'
+else
+  echo '***[ ~/.bash_aliases kopioiminen epäonnistui ]***'
+fi
+
+
+# Tarkistetaan onko kohdehakemistoa olemassa, jos ei niin luodaan linkki
+if [ ! -d "/etc/puppet/modules" ]; then
+ sudo ln -s ~/keskitettyHallinta/modules/ /etc/puppet/
+ echo '***[ /etc/puppet/modules kansio luotu & tehty linkki  ]***'
+else
+ sudo mv -f /etc/puppet/modules/ /etc/puppet/oldModules/
+ sudo ln -s ~/keskitettyHallinta/modules/ /etc/puppet/
+ echo '***[ /etc/puppet/modules kansio nimetty uudelleen & linkki luotu  ]***'
+fi
+
 
 # Refreshataan ~/.bash_aliases
 echo ""
-echo "* Refreshataan ~/.bash_aliases *"
+echo "* Ladataan ~/.bash_aliases uudelleen *"
 echo ""
 source ~/.bash_aliases
+
+echo ""
+echo "*  .:[ VALMIS ]:.  *"
+echo ""
+
--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -9,13 +9,14 @@ class apache {
 	# Kopioidaan apache2 kansioon uusi virtuaalihosti
        file { "/etc/apache2/sites-available/001-kovaluucom.conf":
                 ensure => "file",
-                content => template('apache/001-kovaluu.com.erb'),
+                content => template('apache/001-kovaluucom.erb'),
         }
 
 	# Luodaan linkki apache2 sites-available -> sites-enabled
 	file { "/etc/apache2/sites-enabled/001-kovaluucom.conf":
 		ensure => "link",
-		target => "/etc/apache2/sites-available/001-kovaluucom.conf",
+		target => "../sites-available/001-kovaluucom.conf",
+		notify => Service["apache2"],
 	}
 
 	# Luodaan virtuaalihostille uusi kansio
@@ -26,14 +27,16 @@ class apache {
 	# Kopioidaan virtuaalihostille uusi index.html
 	file { "/var/www/kovaluu.com/index.html":
 		ensure => "file",
-		content => template('apache/index.html.erb'),
-		notify => Service['apache2'],
+		mode => "0644",
+		content => template("apache/index.html.erb"),
+		notify => Service["apache2"],
 	}
 
         # Varmistetaan, että palvelu on varmasti päällä ja käynnistyy automaattisesti
         service { "apache2":
                 enable => "true",
                 ensure => "running",
+		provider => "systemd",
                 require => Package["apache2"],
         }
 }
